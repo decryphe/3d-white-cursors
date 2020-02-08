ee93365bf2c1628d2df46f9f94b6d35503ee67f3
Hans <sjoekvist@hotmail.com>
2020-02-08 02:12:20 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# 3D White Cursors
+
+This is a reimplementation of the classic 3D-White cursors known from Windows 95 and onwards. This reimplementation features many new cursors inspired from the old design to offer a complete set of cursors for use under Linux/X11.
+
+The build script is based on the work done for the [Numix Cursor Theme](https://github.com/numixproject/numix-cursor-theme).
+
+### Installing
+
+I still have to figure out how to offer a package as built via GitHub.
+
+### Building
+
+Building the cursors needs Inkscape and xcursorgen. With those prerequisites installed you can just run the build script. It will generate a tarball that can be installed using for example KDEs mouse cursor settings panel.
+
+Enjoy!
---
+++ build.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
+THEME_SVGS="src/svgs"
+THEME_INDEX="src/index.theme"
+ALIASES="cursorList"
+
+echo -ne "Checking Requirements...\\r"
+if [ ! -f $THEME_INDEX ] ; then
+    echo -e "\\nFAIL: '$THEME_INDEX' missing"
+    exit 1
+elif ! type "inkscape" > /dev/null ; then
+    echo -e "\\nFAIL: inkscape must be installed"
+    exit 1
+elif ! type "xcursorgen" > /dev/null ; then
+    echo -e "\\nFAIL: xcursorgen must be installed"
+    exit 1
+fi
+echo -e "Checking Requirements... DONE"
+
+# Make output directory
+BUILD_DIR="build"
+mkdir -p "$BUILD_DIR/3d-white/cursors"
+
+
+for SIZE in 24 32 48; do
+    echo -ne "\\033[0KGenerating cursors for size $SIZE...\\r"
+    
+    # Make output subdirectory
+    OUTPUT_DIR="$BUILD_DIR/$SIZE"
+    mkdir -p "$OUTPUT_DIR"
+
+    echo -ne "\\033[0KGenerating simple cursor pixmaps...\\r"
+    for CUR in src/config/*.cursor; do
+        BASENAME=$CUR
+        BASENAME=${BASENAME##*/}
+        BASENAME=${BASENAME%.*}
+
+        echo -ne "\\033[0KGenerating $BASENAME @ $SIZE\\r"
+
+        inkscape -w $SIZE -f $THEME_SVGS/"$BASENAME".svg -e "$OUTPUT_DIR/$BASENAME.png" > /dev/null
+    done
+    echo -e "\\033[0KGenerating simple cursor pixmaps... DONE"
+    
+    
+    echo -ne "\\033[0KGenerating progress-cursor pixmaps...\\r"
+    for i in 01 02 03 04 05 06 07 08 09 10; do
+        inkscape -w $SIZE -f $THEME_SVGS/progress-$i.svg -e "$OUTPUT_DIR/progress-$i.png" > /dev/null
+    done
+    echo -e "\\033[0KGenerating progress-cursor pixmaps... DONE"
+    
+    
+    echo -ne "\\033[0KGenerating wait-cursor pixmaps...\\r"
+    for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20; do
+        inkscape -w $SIZE -f $THEME_SVGS/wait-$i.svg -e "$OUTPUT_DIR/wait-$i.png" > /dev/null
+    done
+    echo -e "\\033[0KGenerating wait-cursor pixmaps... DONE"
+done
+
+
+echo -ne "Generating cursor theme...\\r"
+for CUR in src/config/*.cursor; do
+    BASENAME=$CUR
+    BASENAME=${BASENAME##*/}
+    BASENAME=${BASENAME%.*}
+
+    if ! ERR="$( xcursorgen -p build "$CUR" "$BUILD_DIR/3d-white/cursors/$BASENAME" 2>&1 )"; then
+        echo "FAIL: $CUR $ERR"
+    fi
+done
+echo -e "Generating cursor theme... DONE"
+
+
+echo -ne "Generating shortcuts...\\r"
+while read -r ALIAS ; do
+    FROM=${ALIAS% *}
+    TO=${ALIAS#* }
+    if [ -e "$BUILD_DIR/3d-white/cursors/$FROM" ] ; then
+        continue
+    fi
+    ln -sf "$TO" "$BUILD_DIR/3d-white/cursors/$FROM"
+done < $ALIASES
+echo -e "\\033[0KGenerating shortcuts... DONE"
+
+
+echo -ne "Copying Theme Index...\\r"
+    if ! [ -e "$BUILD_DIR/3d-white/index.theme" ] ; then
+        cp $THEME_INDEX "$BUILD_DIR/3d-white/index.theme"
+    fi
+echo -e "\\033[0KCopying Theme Index... DONE"
+
+
+echo -ne "Building Theme archive...\\r"
+    (cd "$BUILD_DIR" && tar -czvf 3d-white.tar.gz 3d-white/)
+echo -e "\\033[0KBuilding Theme archive... DONE"
+
+
+echo "COMPLETE!"
